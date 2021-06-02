6a74e381d9f4f8e2f8fe1e28dbd76f3bcff90ff2
shantocv <shantocv@gmail.com>
2021-06-02 18:32:02 +0530

    register form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :tasks
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tasks#index'
